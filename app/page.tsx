a0501cb62d598d887e5f4d8dc1f4fe2baa961c61
Will Lee <will@willlee.ca>
2023-03-01 15:31:47 -0700

    Fetch data test

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,25 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const getData = async () => {
+  const data = await fetch('https://www.reddit.com/.json')
+  return data.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+  const post = data.data.children[0].data.title
+  
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
+      <Header />
+      <h2>{post}</h2>
+      
       <div className={styles.center}>
         <Image
           className={styles.logo}
